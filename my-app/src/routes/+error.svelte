db77dde9f7c42c2a00ef3a543298306b2c4cf56b
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-01-18 01:11:26 -0800

    Add navbar

---
+++ my-app/src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<h1>An Error Occured</h1>
+<style>
+    h1 {
+        color: red;
+    }
+</style>
---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { children } = $props();
+</script>
+
+<nav>
+    <a href="../">Home</a>
+    <a href="../About">About</a>
+    <a href="../Projects">Projects</a>
+    <a href="../TechStack">Website Tech Stack</a>
+    <a href="../Contact">Contact</a>
+</nav>
+
+{@render children()}
