c4ffc8a6e5f2d51b930bbc562e7a0dbe885db8c6
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-01-26 02:57:39 -0800

    Add global.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
-    import './style.css';
-	let { children } = $props();
+    // global css that the layout will pass on to its children
+    import './global.css';
+    let { children } = $props();
 </script>
+
+<style>
+    /*local css*/
+    h1 {
+        color: magenta;
+    }
+</style>
 
 <nav class="main-navbar">
     <a href="./">Home</a>
@@ -10,6 +18,12 @@
     <a href="./TechStack">Tech Stack</a>
     <a href="./Contact">Contact</a>
 </nav>
+
+<h1>Hello</h1>
+<div class="avatar-box">
+    <img src="./assets/tojiwoohoo.jpg" alt="Profile" class="avatar"/>
+</div>
+
 
 {@render children()}
 
