a5f0c97a833d557fb8091acb8abf7ea8320941ef
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-01-26 02:57:51 -0800

    Add global.css

--- src/routes/TechStack/+page.svelte
+++ src/routes/TechStack/+page.svelte
@@ -1,1 +1,18 @@
+<style>
+    /*local css has priority over global css since this component is using a scoped <style>, not importing */
+    h1 {
+        color: white;
+        text-shadow: cyan 1px 0 10px;
+    }
+
+    h2 {
+        color: white;
+    }
+</style>
+
 <h1>Tech Stack</h1>
+
+<h2>Frontend</h2>
+<ul>
+    <li>SvelteKit</li>
+</ul>
